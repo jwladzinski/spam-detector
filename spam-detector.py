cf9eafb5284175bbe255169a7e6df1d6e58464d6
jacek-w <jacek-w@github.com>
2018-02-27 13:48:16 +0100

    Test MultinomialNB model, print confusion matrix

--- spam-detector.py
+++ spam-detector.py
@@ -4,7 +4,9 @@ import os
 import sys
 import json
 import csv
+import numpy as np
 import pandas as pd
+from pprint import pprint
 from steem import Steem
 from steem.blockchain import Blockchain
 from steem.post import Post
@@ -13,6 +15,7 @@ from steembase.exceptions import PostDoesNotExist
 from textblob import TextBlob
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
 from bs4 import BeautifulSoup
 
 # private posting key from environment variable
@@ -29,33 +32,63 @@ def get_message_from_post(post):
     message = post['body'].strip() 
     return replace_white_spaces_with_space(remove_html_and_markdown(message))
 
-def average_spam_score(model, blog, k=5):   
-    scores = [model.spam_score(get_message_from_post(previous_post)) for previous_post in blog.take(k)]
-    average = sum(scores) / len(scores)
-    return average
+
+
+
 
 # Multinomial Naive Bayes based spam filter trained from input file
 class NaiveBayesSpamFilter:
     def __init__(self, training_file):
         # read data from training file, each row contains label and message separated by '\t'
         self.messages = pd.read_csv(training_file, sep='\t', quoting=csv.QUOTE_NONE, names=['label', 'message'])
-        self.bag_of_words_transformer = CountVectorizer(analyzer=self.split_into_lemmas).fit(self.messages['message'])
-        self.messages_bag_of_words = self.bag_of_words_transformer.transform(self.messages['message'])
+
+        X = self.messages['message']
+        y = self.messages['label']
+
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        self.bag_of_words_transformer = CountVectorizer(analyzer=self.split_into_lemmas).fit(self.X_train)
+        self.messages_bag_of_words = self.bag_of_words_transformer.transform(self.X_train)
         self.tfidf_transformer = TfidfTransformer().fit(self.messages_bag_of_words)
         self.messages_tfidf = self.tfidf_transformer.transform(self.messages_bag_of_words)
         # train Multinomial Naive Bayes algorithm with training data
-        self.multinomial_nb = MultinomialNB().fit(self.messages_tfidf, self.messages['label'])
-      
+        self.multinomial_nb = MultinomialNB().fit(self.messages_tfidf, self.y_train)
+
     # return probability that given message is spam (0.0 - 1.0)
     def spam_score(self, message):
         bag_of_words = self.bag_of_words_transformer.transform([message])
         tfidf = self.tfidf_transformer.transform(bag_of_words)
         return self.multinomial_nb.predict_proba(tfidf)[0][1]
+
+    def average_spam_score(self, blog, k=5):   
+        scores = [self.spam_score(get_message_from_post(previous_post)) for previous_post in blog.take(k)]
+        average = sum(scores) / len(scores)
+        return average
   
     # split text into list of lemmas
     # 'Apples and oranges' -> ['apple', 'and', 'orange']
     def split_into_lemmas(self, message):
         return [word.lemma for word in TextBlob(message.lower()).words]
+
+    def test_model(self, probability_threshold):
+
+        confusion_matrix = [
+        [0, 0],
+        [0, 0]]
+
+        X_test = self.X_test.tolist()
+        y_test = self.y_test.tolist()
+
+        for i, x in enumerate(X_test):
+
+            prediction = 0 if self.spam_score(x) <= probability_threshold else 1
+            label = 0 if y_test[i] == 'ham' else 1
+
+            confusion_matrix[prediction][label] += 1
+
+        print(np.array(confusion_matrix))
+
+
+    
 
 class SpamDetectorBot:
     def __init__(self, config, model):
@@ -98,6 +131,9 @@ class SpamDetectorBot:
         return 'Please stop spamming in comments or else people may flag you!\n<sup>Spam probability: %.2f%% </sup>' % (100 * p)
 
     def run(self):
+
+        self.model.test_model(self.probability_threshold)
+
         blockchain = Blockchain(steemd_instance=self.steem)
         # stream of comments
         stream = blockchain.stream(filter_by=['comment'])
@@ -108,16 +144,18 @@ class SpamDetectorBot:
                     if not post.is_main_post() and post['url'] not in self.seen:
                         main_post = self.main_post(post)
 
-                        if post['author'] in self.whitelist:
-                            print('Ignored:', post['author'])
-                            continue
-
                         # if self.tags is empty bot analyzes all tags
                         # otherwise bot analyzes only comments that contains at least one of given tag          
-                        if True:# not self.tags or (set(self.tags) & set(main_post['tags'])):   
+                        if not self.tags or (set(self.tags) & set(main_post['tags'])):
+
+                            if post['author'] in self.whitelist:
+                                print('Ignored:', post['author'])
+                                continue
+
                             message = get_message_from_post(post) 
                             blog = Blog(account_name=post['author'], comments_only=True, steemd_instance=self.steem)
-                            p = average_spam_score(self.model, blog, 5)
+                            p = self.model.average_spam_score(blog, 5)
+                            print('*' if p > self.probability_threshold else ' ', end='')       
                             self.log(p, post['author'], message)
                             self.seen.add(post['url'])
                             if p > self.probability_threshold:
